f9020e3a429c83af1152e12d07d77b4a9f3b0065
Matt Smith <mattsmith13815@me.com>
2025-05-31 16:27:35 -0400

    Cloudflare Pages to Workers

--- eleventy.config.js
+++ eleventy.config.js
@@ -10,7 +10,7 @@ module.exports = function(eleventyConfig) {
   return {
     dir: {
       input: "src",
-      output: "_site",
+      output: "dist",
       includes: "_includes",
       data: "_data"
     },
